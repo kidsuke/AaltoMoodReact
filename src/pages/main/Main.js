bd0dac2131580cbc72cecb6cd4edbc02d35929b7
Long Vu <longvh96@gmail.com>
2018-10-06 15:52:26 +0300

    Init project

---
+++ src/pages/main/Main.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { ReactMic } from 'react-mic';
+import { Button, ButtonToolbar } from "react-bootstrap"
+
+export default class Main extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            streaming: false
+        }
+    }
+
+    startStreaming = () => {
+        console.log('Start streaming...');
+        this.setState({
+            streaming: true
+        })
+    }
+
+    stopStreaming = () => {
+        console.log('Stop streaming...');
+        this.setState({
+            streaming: false
+        })
+    }
+
+    onDataReceived = (recordedBlob) => {
+        console.log('chunk of real-time data is: ', recordedBlob);
+    }
+
+    render() {
+        return (
+            <div>
+                <ReactMic
+                    record={this.state.streaming}
+                    className="sound-wave"
+                    onData={this.onDataReceived}
+                    strokeColor="#FF9800"
+                    backgroundColor="#FF4081" />
+                <ButtonToolbar>
+                    <Button bsStyle="success" onClick={this.startStreaming}>Start call</Button>
+                    <Button bsStyle="danger" onClick={this.stopStreaming}>End call</Button>
+                </ButtonToolbar>
+            </div>
+        )
+    }
+}
