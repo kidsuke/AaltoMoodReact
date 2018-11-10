d1dbc1d20e8bd37fab41f16e70d6a4c2e4e98e4c
Long Vu <longvh96@gmail.com>
2018-11-10 21:41:10 +0200

    Add animation when tips appear

--- src/containers/tip/TipsContainer.js
+++ src/containers/tip/TipsContainer.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react';
 import './TipsContainer.css'
 import {connect} from "react-redux";
 import Tip from "../../components/tip/Tip";
+import posed, {PoseGroup} from "react-pose"
+
+const Box = posed.div({
+    enter: {
+        y: 0,
+        opacity: 1,
+        delay: 300,
+        transition: {
+            y: { type: 'spring' },
+            default: { duration: 300 }
+        }
+    },
+    exit: {
+        y: 50,
+        opacity: 0,
+        transition: { duration: 150 }
+    }
+});
 
 class TipsContainer extends Component {
     render() {
         const { tips } = this.props;
 
         return (
-            <div className="TipsContainer">
+            <PoseGroup className="TipsContainer">
                 {
-                    tips.map(tip => <Tip className="Tip" key={tip.id} tip={tip}/>)
+                    tips.map(tip =>
+                        <Box key={tip.id}>
+                            <Tip className="Tip" key={tip.id} tip={tip}/>
+                        </Box>
+                    )
                 }
-            </div>
+            </PoseGroup>
         )
     }
 }
